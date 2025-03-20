f48abd33834e2b191b6e69bf11bd5cbf5630a23e
Syahrul Romadhon <rmdhn.syahrul@gmail.com>
2025-03-20 16:51:40 +0700

    change our technology content images

--- src/app/components/sections/OurTechnology.tsx
+++ src/app/components/sections/OurTechnology.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import SectionTitle from "../typography/SectionTitle";
 import Image from "next/image";
 import { getDictionary } from "@/get-dictionary";
-import gpsTracking from "../../../../public/images/gps-tracking.jpg";
-import monitoring from "../../../../public/images/monitoring.jpg";
-import integrated from "../../../../public/images/integrated.jpg";
+import gpsTracking from "../../../../public/images/image-k-1.jpg";
+import monitoring from "../../../../public/images/image-k-2.jpg";
+import integrated from "../../../../public/images/image-k-3.jpg";
+import fuelMonitoring from "../../../../public/images/image-k-4.jpg";
 
 export default function OurTechnology({
   dictionary,
@@ -27,7 +28,11 @@ export default function OurTechnology({
             ],
             [dictionary["tech-title-2"], dictionary["tech-desc-2"], monitoring],
             [dictionary["tech-title-3"], dictionary["tech-desc-3"], integrated],
-            [dictionary["tech-title-4"], dictionary["tech-desc-4"], monitoring],
+            [
+              dictionary["tech-title-4"],
+              dictionary["tech-desc-4"],
+              fuelMonitoring,
+            ],
           ].map(([title, description, imageUrl], index) => (
             <div
               className="grid grid-flow-row sm:grid-cols-4 gap-[30px] mb-[48px] animate-fade-in [animation-timeline:view()] [animation-range-start:cover] [animation-range-end:500px]"
