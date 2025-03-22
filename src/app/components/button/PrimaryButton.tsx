604655dc212588ffe2a914ff09c5bb67f4cbeea5
Syahrul Romadhon <rmdhn.syahrul@gmail.com>
2025-03-22 22:09:52 +0700

    feat(component): loading spinner

--- src/app/components/button/PrimaryButton.tsx
+++ src/app/components/button/PrimaryButton.tsx
@@ -1,15 +1,23 @@
 import React, { ButtonHTMLAttributes } from "react";
+import LoadingSpinner from "../loading/loading-spinner";
 
+export interface BaseButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
+  loading?: boolean;
+  loadingTitle?: string;
+}
 export default function PrimaryButton({
   children,
+  loading = false,
+  loadingTitle = "Sending...",
   ...restProps
-}: ButtonHTMLAttributes<HTMLButtonElement>) {
+}: BaseButtonProps) {
   return (
     <button
       className="inline-block transition-all duration-500 bg-blue-500 py-4 sm:py-[18px] px-3 sm:px-6 rounded-xl hover:bg-blue-100 hover:-translate-y-1.5 disabled:bg-disabled font-bold text-white"
       {...restProps}
     >
-      {children}
+      {loading ? <LoadingSpinner title={loadingTitle} /> : children}
     </button>
   );
 }
--- src/app/components/button/SecondaryButton.tsx
+++ src/app/components/button/SecondaryButton.tsx
@@ -1,15 +1,18 @@
 import React, { ButtonHTMLAttributes } from "react";
+import { BaseButtonProps } from "./PrimaryButton";
+import LoadingSpinner from "../loading/loading-spinner";
 
 export default function SecondaryButton({
   children,
+  loading = false,
   ...restProps
-}: ButtonHTMLAttributes<HTMLButtonElement>) {
+}: BaseButtonProps) {
   return (
     <button
       className="inline-block transition-all duration-500 bg-green-500 py-[18px] px-6 rounded-xl hover:bg-green-100  hover:-translate-y-1.5 disabled:bg-disabled font-bold text-white"
       {...restProps}
     >
-      {children}
+      {loading ? <LoadingSpinner /> : children}
     </button>
   );
 }
