5cf088aeb6deb114c7960601f126f2e82e7465ec
Syahrul Romadhon <rmdhn.syahrul@gmail.com>
2025-03-24 00:12:53 +0700

    fix: faq answer not fully appears on mobile device

--- src/app/components/accordion/Accordion.tsx
+++ src/app/components/accordion/Accordion.tsx
@@ -8,7 +8,7 @@ export default function Accordion({ question, answer }: Props) {
   return (
     <label>
       <input className="peer/showLabel absolute scale-0" type="checkbox" />
-      <span className="block max-h-20 sm:max-h-16 overflow-hidden rounded-lg bg-white px-6 py-1.5 text-primary-black transition-all duration-300 peer-checked/showLabel:bg-primary-blue peer-checked/showLabel:text-white peer-checked/showLabel:max-h-full ">
+      <span className="block max-h-[90px] sm:max-h-16 overflow-hidden rounded-lg bg-white px-6 py-1.5 text-primary-black transition-all duration-300 peer-checked/showLabel:bg-primary-blue peer-checked/showLabel:text-white peer-checked/showLabel:max-h-full ">
         <div className="flex h-20 sm:h-14 cursor-pointer items-center font-bold justify-between">
           <h3>{question}</h3>
           <ChevronDown />
